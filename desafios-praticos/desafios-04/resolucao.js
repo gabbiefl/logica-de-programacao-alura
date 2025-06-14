55d052ade7bb1d593c2e67e264a88ccc17b91d62
gabbiefl <gabi.flores@outlook.com>
2025-06-14 16:23:41 -0300

    resolucao da lista de exercicios-04

--- desafios-praticos/desafios-04/resolucao.js
+++ desafios-praticos/desafios-04/resolucao.js
@@ -1,11 +1,27 @@
-// Crie uma lista vazia, com o nome listaGenerica.
+// 1. Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
 
-// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+// 2. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
 
-// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+console.log(linguagensDeProgramacao);
 
-// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+// 3. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
 
-// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+console.log(linguagensDeProgramacao);
 
-// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+// 4. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomes1 = ["Maria", "Ana", "João"];
+
+console.log(nomes1[0]);
+
+// 5. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let nomes2 = ["Elisandra", "Melina", "Gabriela"];
+
+console.log(nomes2[1]);
+
+// 6. Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let nomes3 = ["Paulo", "Carlos", "Janilson"];
+
+console.log(nomes3[nomes3.length - 1]); // console.log(nomes3[2])
